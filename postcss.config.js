668ad1f1882fde7c6fa18e5fb1d17ffef7799b28
bill.mark <weiguicheng@foxmail.com>
2018-06-08 17:22:36 +0800

    配置jsx

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+ const autoprefixer = require('autoprefixer')
+
+ module.exports = {
+     plugins: [
+         autoprefixer()  //给CSS属性加前缀
+     ]
+ }
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ const config = {
                 loader:'vue-loader'
             },
             {
+                test: /\.jsx$/,
+                loader: 'babel-loader'
+            },
+            {
                 test:/\.css$/,
                 use:[
                     'style-loader',
@@ -29,6 +33,12 @@ const config = {
                 use: [
                     'style-loader',
                     'css-loader',
+                    {
+                        loader:'postcss-loader',
+                        options: {
+                            sourceMap: true,//stylus-loader已经生成了sourcemap,配置为true,避免再生成一次
+                        }
+                    },
                     'stylus-loader'
                 ]
             },
